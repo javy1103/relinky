a8e3281f77d1cb3b1de97d6bd29720a18e8a300e
Javier <javy1103@gmail.com>
2016-09-27 00:47:59 -0500

    converted search form to a partial template

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -77,73 +77,7 @@
 
     <section style="background-image: url('img/slider/car.jpg')" class="banner" id="sect-overview">
         <div class="uk-grid uk-grid-width-1-1">
-            <div class="search-form">
-                <div class="md-card">
-                    <div class="md-card-toolbar">
-                        <div class="md-card-toolbar-actions">
-                            <ul class="uk-tab" data-uk-tab="{connect:'#card_tabs',animation:'slide-horizontal'}">
-                                <li class="uk-active" aria-expanded="true"><a href="#">Buy</a></li>
-                                <li aria-expanded="false" class=""><a href="#">Rent</a></li>
-                                <li aria-expanded="false" class=""><a href="#">Sell</a></li>
-                                <li aria-expanded="false" class=""><a href="#">Find an Agent</a></li>
-                            </ul>
-                        </div>
-                        <h2 class="md-card-toolbar-heading-text">
-                            Looking to:
-                        </h2>
-                    </div>
-                    <div class="md-card-content">
-                        <ul id="card_tabs" class="uk-switcher uk-margin">
-                            <li aria-hidden="false" class="uk-active" style="animation-duration: 200ms;">
-                                <div class="uk-grid">
-                                    <div class="uk-width-large-1-1 uk-width-1-1">
-                                        <div class="uk-input-group">
-                                            <span class="uk-input-group-addon"><i class="uk-input-group-icon uk-icon-search"></i></span>
-                                            <div class="md-input-wrapper"><input class="md-input" type="text" id="rent" placeholder="Enter address, city, or zipcode"></div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                            <li aria-hidden="true" class="" style="animation-duration: 200ms;">
-                                <div class="uk-grid">
-                                    <div class="uk-width-large-1-1 uk-width-1-1">
-                                        <div class="uk-input-group">
-                                            <span class="uk-input-group-addon"><i class="uk-input-group-icon uk-icon-search"></i></span>
-                                            <div class="md-input-wrapper">
-                                                <input class="md-input" type="text" id="buy" placeholder="Enter address, city, or zipcode">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                            <li aria-hidden="true" class="" style="animation-duration: 200ms;">
-                                <div class="uk-grid">
-                                    <div class="uk-width-large-1-1 uk-width-1-1">
-                                        <div class="uk-input-group">
-                                            <span class="uk-input-group-addon"><i class="uk-input-group-icon uk-icon-search"></i></span>
-                                            <div class="md-input-wrapper">
-                                                <input class="md-input" type="text" id="buy" placeholder="Enter address, city, or zipcode">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                            <li aria-hidden="true" class="" style="animation-duration: 200ms;">
-                                <div class="uk-grid">
-                                    <div class="uk-width-large-1-1 uk-width-1-1">
-                                        <div class="uk-input-group">
-                                            <span class="uk-input-group-addon"><i class="uk-input-group-icon uk-icon-search"></i></span>
-                                            <div class="md-input-wrapper">
-                                                <input class="md-input" type="text" id="agent" placeholder="Enter name, city, or zipcode">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+            @include('shared.forms.basic_search')
         </div>
     </section>
 
