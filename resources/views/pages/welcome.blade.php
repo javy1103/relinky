0f16c15e3ad00e5c5092aa1adbfa93b641907b0f
Javier <javy1103@gmail.com>
2016-10-03 19:54:11 -0500

    saving work

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -23,7 +23,7 @@
                             </a>
                         </li>
                         <li>
-                            <a href="{{url('login')}}">
+                            <a href="#login_modal" data-uk-modal>
                                 Login
                             </a>
                         </li>
@@ -575,4 +575,31 @@
     </div>
 </div>
 </section>
+
+<div class="uk-modal" id="login_modal" aria-hidden="true" style="display: none; overflow-y: auto;">
+    <div class="uk-modal-dialog login-modal">
+        <button type="button" class="uk-modal-close uk-close"></button>
+        @include('shared.login_form')
+        <div class="md-card-content large-padding uk-position-relative" id="login_help" style="display: none">
+            <button type="button" class="uk-position-top-right uk-close uk-margin-right uk-margin-top back_to_login"></button>
+            <h2 class="heading_b uk-text-success">Can't log in?</h2>
+            <p>Here’s the info to get you back in to your account as quickly as possible.</p>
+            <p>First, try the easiest thing: if you remember your password but it isn’t working, make sure that Caps Lock is turned off, and that your username is spelled correctly, and then try again.</p>
+            <p>If your password still isn’t working, it’s time to <a href="#" id="password_reset_show">reset your password</a>.</p>
+        </div>
+        <div class="md-card-content large-padding" id="login_password_reset" style="display: none">
+            <button type="button" class="uk-position-top-right uk-close uk-margin-right uk-margin-top back_to_login"></button>
+            <h2 class="heading_a uk-margin-large-bottom">Reset password</h2>
+            <form>
+                <div class="uk-form-row">
+                    <label for="login_email_reset">Your email address</label>
+                    <input class="md-input" type="text" id="login_email_reset" name="login_email_reset" />
+                </div>
+                <div class="uk-margin-medium-top">
+                    <a href="index.html" class="md-btn md-btn-primary md-btn-block">Reset password</a>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 @endsection
--- resources/views/shared/login_form.blade.php
+++ resources/views/shared/login_form.blade.php
@@ -1,6 +1,6 @@
 <div class="md-card-content large-padding" id="login_form">
     <div class="login_heading">
-        <div class="user_avatar"></div>
+        <h2 class="uk-text-center">Sign In</h2>
     </div>
     <form role="form" method="POST" action="{{ url('/login') }}">
         {{ csrf_field() }}
@@ -20,11 +20,6 @@
         </div>
         <div class="uk-margin-medium-top">
             <button type="submit" class="md-btn md-btn-primary md-btn-block md-btn-large">Sign In</button>
-        </div>
-        <div class="uk-grid uk-grid-width-1-3 uk-grid-small uk-margin-top">
-            <div><a href="#" class="md-btn md-btn-block md-btn-facebook" data-uk-tooltip="{pos:'bottom'}" title="Sign in with Facebook"><i class="uk-icon-facebook uk-margin-remove"></i></a></div>
-            <div><a href="#" class="md-btn md-btn-block md-btn-twitter" data-uk-tooltip="{pos:'bottom'}" title="Sign in with Twitter"><i class="uk-icon-twitter uk-margin-remove"></i></a></div>
-            <div><a href="#" class="md-btn md-btn-block md-btn-gplus" data-uk-tooltip="{pos:'bottom'}" title="Sign in with Google+"><i class="uk-icon-google-plus uk-margin-remove"></i></a></div>
         </div>
         <div class="uk-margin-top">
             <a href="#" id="login_help_show" class="uk-float-right">Need help?</a>
